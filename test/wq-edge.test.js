89c9dc2014dba927237b697c1539f1f6e809c4c8
JP Richardson <jprichardson@gmail.com>
2013-10-15 13:50:27 -0500

    allow user to modify data before calling done() on dequeued item

--- test/wq-edge.test.js
+++ test/wq-edge.test.js
@@ -1,4 +1,5 @@
 var wq = require('../lib/wq.js')
+  , dq = require('dq')
   , testutil = require('testutil')
   , batch = require('batchflow')
   , stats = require('../lib/stats')
@@ -33,5 +34,45 @@ describe('wq', function() {
         })
       })
     })
+  
+    describe('> when the enqueuing strings', function() {
+      it('should allow the user to modify the string or object and save to done queue', function(done) {
+        var data = ['JP', 'Leslie', 'Chris']
+        var outputData = []
+        batch(data).seq().each(function(i, item, next) {
+          q.enq(item, i, next)
+        })
+        .error(done)
+        .end(function() {
+          batch(data).seq().each(function(i, item, next) {
+            q.deq(function(err, item) {
+              if (err) return done(err)
+              item.data = {name: item.data} //create object
+              item.done(next)
+            })
+          })
+          .error(done)
+          .end(function() {
+            dq.create({name: 'wq:done:wqtest'}, function(err, dataq) {
+              if (err) return done(err)
+              batch(data).seq().each(function(i, item, next) {
+                dataq.deq(function(err, item) { //item is now raw string representation from dq
+                  outputData.push(JSON.parse(item))
+                  next()
+                })
+              })
+              .error(done)
+              .end(function() {
+                data.forEach(function(name) {
+                  T (outputData.some(function(i) { return i.name === name}))
+                })
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
   })
 })
+
